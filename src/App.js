fc984213b1d2e59314c4f70cdbd4ca2d448c2bcd
Mahmoud Awwad <mabdelssamea@gmail.com>
2022-04-14 00:20:08 +0300

    initalize react app setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,4 @@
-import { Route, Switch } from "react-router";
-import AllMeetUp from "./pages/AllMeetUp";
-import NewMeetUp from "./pages/NewMeetUp";
-import Favorites from "./pages/Favorites";
-import Layout from "./commponents/layout/Layout";
-
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetUp />
-        </Route>
-        <Route path="/newmeetup">
-          <NewMeetUp />
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+  return <>hello there</>;
 }
 export default App;
--- src/pages/Favorites.js
+++
@@ -1,9 +0,0 @@
-function Favorites(props) {
-  return (
-    <div>
-      <p>Favorites page</p>
-    </div>
-  );
-}
-
-export default Favorites;
